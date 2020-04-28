aaf1815191384e9bd9e3ab6619a4de822b729077
Erce Kal <erce@futr.ai>
2020-04-28 13:15:22 +0100

    pan gesture is done

--- src/Deck.js
+++ src/Deck.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import { Text, View, Animated } from 'react-native';
+import React, {useState} from 'react';
+import {
+  Text,
+  View,
+  Animated,
+  PanResponder
+ } from 'react-native';
 
 const Deck = ({data, renderCard}) => {
+  const position = new Animated.ValueXY()
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: (event, gesture) => {
+      position.setValue({x: gesture.dx, y: gesture.dy})
+    },
+    onPanResponderRelease: () => {}
+  })
+  const [panResponderState, setPanResponderState] = useState(panResponder)
   const renderCards = () => {
     return data.map(item => {
       return renderCard(item)
     })
   }
  return (
-    <View>
+    <Animated.View
+      style={position.getLayout()}
+      {...panResponderState.panHandlers}>
       {renderCards()}
-    </View>
-)
+    </Animated.View>
+  )
 }
 
 export default Deck;
